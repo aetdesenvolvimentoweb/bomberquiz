93dee3ca398ecffcc2f2d2a7c4fb3a722c233254
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-14 19:10:00 -0300

    test: ensure that AddMilitaryRankController return 400 if already registered name is provided

--- __tests__/modules/backend/presentation/controllers/military-rank/add-military-rank-controller.spec.ts
+++ __tests__/modules/backend/presentation/controllers/military-rank/add-military-rank-controller.spec.ts
@@ -1,7 +1,10 @@
 import { AddMilitaryRankService } from "@/modules/backend/data/usecases/military-rank";
 import { MilitaryRanksInMemoryRepository } from "@/modules/backend/infra/db/in-memory";
 import { AddMilitaryRankController } from "@/modules/backend/presentation/controllers/military-ranks";
-import { MissingParamError } from "@/modules/backend/presentation/errors";
+import {
+  DuplicatedKeyError,
+  MissingParamError,
+} from "@/modules/backend/presentation/errors";
 
 interface SutResponse {
   sut: AddMilitaryRankController;
@@ -38,6 +41,22 @@ describe("AddMilitaryRankController", () => {
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.error).toEqual(new MissingParamError("nome").message);
+  });
+  test("should be return 400 if duplicated name is provided", async () => {
+    const { sut } = makeSut();
+
+    await sut.handle({
+      order: 1,
+      name: "any_military_rank",
+    });
+
+    const httpResponse = await sut.handle({
+      order: 2,
+      name: "any_military_rank",
+    });
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.error).toEqual(new DuplicatedKeyError("nome").message);
   });
   test("should be return 201 if correct data is provided", async () => {
     const { sut } = makeSut();
