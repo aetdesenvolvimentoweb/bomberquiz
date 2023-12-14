dc09255b922c5b3e8ac2cbe52c5e704ed926e68e
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-14 16:29:12 -0300

    feat: create military ranks in memory repository

---
+++ src/modules/backend/infra/db/in-memory/military-ranks-repository.ts
@@ -0,0 +1,25 @@
+import { MilitaryRankRepository } from "@/modules/backend/data/protocols/repositories";
+import {
+  AddMilitaryRankModel,
+  MilitaryRankModel,
+} from "@/modules/backend/domain/models";
+import { randomUUID } from "crypto";
+
+export class MilitaryRanksInMemoryRepository implements MilitaryRankRepository {
+  private readonly militaryRanks: MilitaryRankModel[];
+
+  constructor() {
+    this.militaryRanks = [];
+  }
+
+  add = async (data: AddMilitaryRankModel): Promise<MilitaryRankModel> => {
+    const militaryRank = { ...data, id: randomUUID() };
+    this.militaryRanks.push(militaryRank);
+    return militaryRank;
+  };
+
+  getByName = async (name: string): Promise<MilitaryRankModel | null> => {
+    const militaryRank = this.militaryRanks.find((rank) => rank.name === name);
+    return militaryRank || null;
+  };
+}
