2c972daf6bb2e48c56b37cf48dfde72e50af224a
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-14 10:13:06 -0300

    refactor: create structure folders to modules backend and frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import "@/styles/globals.css";
+import "@/modules/frontend/styles/globals.css";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={roboto.className}>{children}</body>
+      <body className={`${roboto.className}`}>{children}</body>
     </html>
   );
 }
