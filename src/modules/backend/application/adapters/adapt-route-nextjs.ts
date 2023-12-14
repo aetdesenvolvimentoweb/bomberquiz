ec9877da825ef6f7d534915beea31802428cc95c
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-14 18:08:37 -0300

    feat: create adapter to nextjs routes

---
+++ src/modules/backend/application/adapters/adapt-route-nextjs.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { Controller } from "../../presentation/protocols";
+
+export const adaptRouteNextjs = async (
+  controller: Controller,
+  request: Request
+) => {
+  const data = await request.json();
+  const httpResponse = await controller.handle(data);
+
+  if (httpResponse.error) {
+    return NextResponse.json(
+      { error: httpResponse.error },
+      { status: httpResponse.statusCode }
+    );
+  } else {
+    return NextResponse.json(
+      { data: httpResponse.data },
+      { status: httpResponse.statusCode }
+    );
+  }
+};
