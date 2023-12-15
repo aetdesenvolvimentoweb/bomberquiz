9ad5b9716bab5a0b9a5e04e6ecd78c1faae2f52c
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-15 14:53:52 -0300

    feat: add get by id to military rank repository protocol

--- src/modules/backend/data/protocols/repositories/military-rank-repository.ts
+++ src/modules/backend/data/protocols/repositories/military-rank-repository.ts
@@ -6,5 +6,6 @@ import {
 export interface MilitaryRankRepository {
   add: (data: AddMilitaryRankModel) => Promise<MilitaryRankModel>;
   getAll: () => Promise<MilitaryRankModel[]>;
+  getById: (id: string) => Promise<MilitaryRankModel | null>;
   getByName: (name: string) => Promise<MilitaryRankModel | null>;
 }
