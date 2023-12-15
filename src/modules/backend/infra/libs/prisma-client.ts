2b2af745e6cb9cf8a26e34e9dbdeaa166e65baee
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-14 21:14:16 -0300

    chore: add prisma client

---
+++ src/modules/backend/infra/libs/prisma-client.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prismaClient = globalPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prismaClient;
+
+export default prismaClient;
