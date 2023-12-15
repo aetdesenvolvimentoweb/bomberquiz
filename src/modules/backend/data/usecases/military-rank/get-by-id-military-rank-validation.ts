8c7ae1db79b49dabd1a237051f415a61f7c9ba21
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-15 15:35:32 -0300

    refactor: remove errors from data layer

--- src/modules/backend/data/usecases/military-rank/get-by-id-military-rank-validation.ts
+++ src/modules/backend/data/usecases/military-rank/get-by-id-military-rank-validation.ts
@@ -1,13 +1,12 @@
 import { IdValidation } from "@/modules/backend/domain/validations";
-import { InvalidParamError } from "@/modules/backend/presentation/errors/invalid-param-error";
 
 export class GetByIdMilitaryRankValidation implements IdValidation {
   constructor(private readonly idValidator: IdValidation) {}
 
   checkIdIsValid = (id: string): boolean => {
-    if (!this.idValidator.checkIdIsValid(id)) {
-      throw new InvalidParamError("id");
+    if (this.idValidator.checkIdIsValid(id)) {
+      return true;
     }
-    return true;
+    return false;
   };
 }
