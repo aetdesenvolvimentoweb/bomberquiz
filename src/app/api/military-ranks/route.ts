8c16898facb209a1922a836efb690f83cf73d2c9
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-15 10:21:03 -0300

    feat: ensure that get method of military api route return 200 if correct data is provided

--- src/app/api/military-ranks/route.ts
+++ src/app/api/military-ranks/route.ts
@@ -1,6 +1,13 @@
 import { adaptRouteNextjs } from "@/modules/backend/application/adapters";
-import { makeAddMilitaryRankController } from "@/modules/backend/application/factories/controllers";
+import {
+  makeAddMilitaryRankController,
+  makeGetAllMilitaryRankController,
+} from "@/modules/backend/application/factories/controllers";
 
 export const POST = async (request: Request) => {
   return await adaptRouteNextjs(makeAddMilitaryRankController(), request);
+};
+
+export const GET = async (request: Request) => {
+  return await adaptRouteNextjs(makeGetAllMilitaryRankController(), request);
 };
