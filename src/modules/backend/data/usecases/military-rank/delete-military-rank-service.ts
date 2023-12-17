019ebf88d59a9a32bb9d4095c46321c05c0b9dde
André David <aetdesenvolvimentoweb@gmail.com>
2023-12-17 09:31:06 -0300

    feat: create service to delete a military rank

---
+++ src/modules/backend/data/usecases/military-rank/delete-military-rank-service.ts
@@ -0,0 +1,25 @@
+import { DeleteMilitaryRankUsecase } from "@/modules/backend/domain/usecases/delete-military-rank-usecase";
+import { MilitaryRankRepository } from "../../protocols/repositories";
+import { IdValidation } from "@/modules/backend/domain/validations";
+import { MilitaryRankModel } from "@/modules/backend/domain/models";
+import { DeleteMilitaryRankValidation } from "./delete-military-rank-validation";
+
+export class DeleteMilitaryRankService implements DeleteMilitaryRankUsecase {
+  private readonly deleteMilitaryRankValidation: DeleteMilitaryRankValidation;
+
+  constructor(
+    private readonly militaryRankRepository: MilitaryRankRepository,
+    private readonly idValidator: IdValidation
+  ) {
+    this.deleteMilitaryRankValidation = new DeleteMilitaryRankValidation(
+      militaryRankRepository
+    );
+  }
+
+  delete = async (id: string): Promise<MilitaryRankModel> => {
+    await this.deleteMilitaryRankValidation.checkMissingParams(id);
+    this.idValidator.isValid(id);
+
+    return await this.militaryRankRepository.delete(id);
+  };
+}
