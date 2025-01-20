7653997689988fb262170fc2abdf0006d98fd264
André David dos Santos <andredavid1@yahoo.com.br>
2025-01-20 14:02:58 -0300

    test: ensure that ListUserByIdService list a user by id

---
+++ src/backend/data/__tests__/services/users/list-by-id.spec.ts
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  IdValidatorUseCase,
+  UserIdValidatorUseCase,
+} from "@/backend/domain/use-cases";
+import { IdValidatorStub } from "@/backend/data/__mocks__";
+import { ListUserByIdService } from "@/backend/data/services";
+import { UserIdValidator } from "@/backend/data/validators";
+import { UserProps } from "@/backend/domain/entities";
+import { UserRepository } from "@/backend/data/repositories";
+import { UserRepositoryInMemory } from "@/backend/infra/in-memory-repositories";
+import { ValidationErrors } from "@/backend/data/helpers";
+
+interface SutTypes {
+  sut: ListUserByIdService;
+  idValidator: IdValidatorUseCase;
+  userRepository: UserRepository;
+  validationErrors: ValidationErrors;
+}
+
+const makeSut = (): SutTypes => {
+  const idValidator = new IdValidatorStub();
+  const userRepository = new UserRepositoryInMemory();
+  const validationErrors = new ValidationErrors();
+  const userIdValidator: UserIdValidatorUseCase = new UserIdValidator({
+    idValidator,
+    userRepository,
+    validationErrors,
+  });
+  const sut = new ListUserByIdService({
+    userRepository,
+    userIdValidator,
+  });
+
+  return {
+    sut,
+    idValidator,
+    userRepository,
+    validationErrors,
+  };
+};
+
+describe("ListUserByIdService", () => {
+  let sut: ListUserByIdService;
+  let idValidator: IdValidatorUseCase;
+  let userRepository: UserRepository;
+  let validationErrors: ValidationErrors;
+  const createUserProps: UserProps = {
+    name: "any_name",
+    email: "valid_email",
+    phone: "any_phone",
+    birthdate: new Date(),
+    role: "cliente",
+    password: "any_password",
+  };
+
+  beforeEach(async () => {
+    const sutInstance = makeSut();
+    sut = sutInstance.sut;
+    idValidator = sutInstance.idValidator;
+    userRepository = sutInstance.userRepository;
+    validationErrors = sutInstance.validationErrors;
+  });
+
+  test("should list a user by id", async () => {
+    await userRepository.create(createUserProps);
+    const user = await userRepository.listByEmail(createUserProps.email);
+
+    await expect(sut.listById(user!.id)).resolves.not.toThrow();
+  });
+});
