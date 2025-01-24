0cd5f9831fa12010b23aa50706fe16a49e39e421
André David dos Santos <andredavid1@yahoo.com.br>
2025-01-24 14:49:05 -0300

    test: ensure that ListAllUsersController return 200 if users was listed

---
+++ src/backend/presentation/controllers/__tests__/users/list-all.spec.ts
@@ -0,0 +1,70 @@
+import { HttpRequest, HttpResponse } from "@/backend/presentation/protocols";
+import { UserMapped, UserProps } from "@/backend/domain/entities";
+import { HttpResponses } from "@/backend/presentation/helpers";
+import { ListAllUsersController } from "@/backend/presentation/controllers";
+import { ListAllUsersService } from "@/backend/data/services";
+import { UserRepository } from "@/backend/data/repositories";
+import { UserRepositoryInMemory } from "@/backend/infra/in-memory-repositories";
+
+interface SutTypes {
+  sut: ListAllUsersController;
+  httpResponses: HttpResponses;
+  userRepository: UserRepository;
+}
+
+const makeSut = (): SutTypes => {
+  const userRepository: UserRepository = new UserRepositoryInMemory();
+  const listAllUsersService: ListAllUsersService = new ListAllUsersService({
+    userRepository,
+  });
+  const httpResponses = new HttpResponses();
+  const sut = new ListAllUsersController({
+    listAllUsersService,
+    httpResponses,
+  });
+
+  return {
+    sut,
+    httpResponses,
+    userRepository,
+  };
+};
+
+describe("ListAllUsersController", () => {
+  let sut: ListAllUsersController;
+  let httpResponses: HttpResponses;
+  let userRepository: UserRepository;
+
+  const createUserProps = (overrides: Partial<UserProps> = {}): UserProps => {
+    return {
+      name: "any_name",
+      email: "valid_email",
+      phone: "any_phone",
+      birthdate: new Date(),
+      role: "cliente",
+      password: "any_password",
+      ...overrides,
+    };
+  };
+
+  beforeEach(() => {
+    const sutInstance = makeSut();
+    sut = sutInstance.sut;
+    httpResponses = sutInstance.httpResponses;
+    userRepository = sutInstance.userRepository;
+  });
+
+  test("should return 200 if users was listed", async () => {
+    await userRepository.create(createUserProps());
+
+    const httpRequest: HttpRequest = {
+      body: {},
+    };
+
+    const httpResponse: HttpResponse<UserMapped[]> =
+      await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse).toEqual(httpResponses.ok(httpResponse.body.data));
+  });
+});
