d2122366f577023719d3b643f39c9098e2936f55
André David dos Santos <andredavid1@yahoo.com.br>
2025-01-24 16:48:41 -0300

    test: ensure that UpdateUserProfileController return 204 if user profile was updated

---
+++ src/backend/presentation/controllers/__tests__/users/update-profile.spec.ts
@@ -0,0 +1,134 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  DateValidatorStub,
+  EmailValidatorStub,
+  IdValidatorStub,
+  PhoneValidatorStub,
+} from "@/backend/data/__mocks__";
+import {
+  DateValidatorUseCase,
+  EmailValidatorUseCase,
+  IdValidatorUseCase,
+  PhoneValidatorUseCase,
+} from "@/backend/domain/use-cases";
+import { HttpRequest, HttpResponse } from "@/backend/presentation/protocols";
+import {
+  UpdateProfilePropsValidator,
+  UserIdValidator,
+} from "@/backend/data/validators";
+import { UserProfile, UserProps } from "@/backend/domain/entities";
+import { HttpResponses } from "@/backend/presentation/helpers";
+import { UpdateUserProfileController } from "@/backend/presentation/controllers";
+import { UpdateUserProfileService } from "@/backend/data/services";
+import { UserRepository } from "@/backend/data/repositories";
+import { UserRepositoryInMemory } from "@/backend/infra/in-memory-repositories";
+import { ValidationErrors } from "@/backend/data/helpers";
+
+interface SutTypes {
+  sut: UpdateUserProfileController;
+  dateValidator: DateValidatorUseCase;
+  emailValidator: EmailValidatorUseCase;
+  idValidator: IdValidatorUseCase;
+  httpResponses: HttpResponses;
+  phoneValidator: PhoneValidatorUseCase;
+  userRepository: UserRepository;
+  validationErrors: ValidationErrors;
+}
+
+const makeSut = (): SutTypes => {
+  const dateValidator: DateValidatorUseCase = new DateValidatorStub();
+  const emailValidator: EmailValidatorUseCase = new EmailValidatorStub();
+  const phoneValidator: PhoneValidatorUseCase = new PhoneValidatorStub();
+  const idValidator: IdValidatorUseCase = new IdValidatorStub();
+  const userRepository: UserRepository = new UserRepositoryInMemory();
+  const validationErrors = new ValidationErrors();
+  const userIdValidator = new UserIdValidator({
+    idValidator,
+    userRepository,
+    validationErrors,
+  });
+  const updateProfilePropsValidator = new UpdateProfilePropsValidator({
+    dateValidator,
+    emailValidator,
+    phoneValidator,
+    userRepository,
+    validationErrors,
+  });
+  const updateUserProfileService: UpdateUserProfileService =
+    new UpdateUserProfileService({
+      updateProfilePropsValidator,
+      userIdValidator,
+      userRepository,
+    });
+  const httpResponses = new HttpResponses();
+  const sut = new UpdateUserProfileController({
+    updateUserProfileService,
+    httpResponses,
+  });
+
+  return {
+    sut,
+    dateValidator,
+    emailValidator,
+    idValidator,
+    httpResponses,
+    phoneValidator,
+    userRepository,
+    validationErrors,
+  };
+};
+
+describe("UpdateUserProfileController", () => {
+  let sut: UpdateUserProfileController;
+  let dateValidator: DateValidatorUseCase;
+  let emailValidator: EmailValidatorUseCase;
+  let idValidator: IdValidatorUseCase;
+  let httpResponses: HttpResponses;
+  let phoneValidator: PhoneValidatorUseCase;
+  let userRepository: UserRepository;
+  let validationErrors: ValidationErrors;
+
+  const createUserProps = (overrides: Partial<UserProps> = {}): UserProps => {
+    return {
+      name: "any_name",
+      email: "valid_email",
+      phone: "any_phone",
+      birthdate: new Date(),
+      role: "cliente",
+      password: "any_password",
+      ...overrides,
+    };
+  };
+
+  beforeEach(() => {
+    const sutInstance = makeSut();
+    sut = sutInstance.sut;
+    dateValidator = sutInstance.dateValidator;
+    emailValidator = sutInstance.emailValidator;
+    idValidator = sutInstance.idValidator;
+    httpResponses = sutInstance.httpResponses;
+    phoneValidator = sutInstance.phoneValidator;
+    userRepository = sutInstance.userRepository;
+    validationErrors = sutInstance.validationErrors;
+  });
+
+  test("should return 204 if user profile was updated", async () => {
+    await userRepository.create(createUserProps());
+    const user = await userRepository.listByEmail(createUserProps().email);
+
+    const httpRequest: HttpRequest<UserProfile> = {
+      body: {
+        id: user!.id,
+        name: "new_name",
+        email: "new_email",
+        phone: "new_phone",
+        birthdate: new Date(),
+      },
+    };
+
+    const httpResponse: HttpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(204);
+    expect(httpResponse).toEqual(httpResponses.noContent());
+  });
+});
