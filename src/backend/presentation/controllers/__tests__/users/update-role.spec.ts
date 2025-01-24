c3dbc8d39d6e9c18a646cabd61f468635caa4732
André David dos Santos <andredavid1@yahoo.com.br>
2025-01-24 15:38:14 -0300

    test: ensure that UpdateUserRoleController return 400 if no id is provided

--- src/backend/presentation/controllers/__tests__/users/update-role.spec.ts
+++ src/backend/presentation/controllers/__tests__/users/update-role.spec.ts
@@ -97,4 +97,18 @@ describe("UpdateUserRoleController", () => {
     expect(httpResponse.statusCode).toBe(204);
     expect(httpResponse).toEqual(httpResponses.noContent());
   });
+
+  test("should return 400 if no id is provided", async () => {
+    const httpRequest: HttpRequest = {
+      body: {},
+      dynamicParams: {},
+    };
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body.error).toBe(
+      validationErrors.missingParamError("id").message
+    );
+  });
 });
