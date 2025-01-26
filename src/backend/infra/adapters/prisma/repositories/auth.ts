e987c3d0a4147da2995ff361c6eab2dda54ea9a2
André David dos Santos <andredavid1@yahoo.com.br>
2025-01-25 22:59:53 -0300

    feat: create PrismaAuthRepositoryAdapter

---
+++ src/backend/infra/adapters/prisma/repositories/auth.ts
@@ -0,0 +1,33 @@
+import { AuthRepository, UserRepository } from "@/backend/data/repositories";
+import { LoginProps, UserLogged } from "@/backend/domain/entities";
+import { db } from "../prisma-client";
+import { prismaConnectionError } from "@/backend/infra/helpers";
+
+export class PrismaAuthRepositoryAdapter implements AuthRepository {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  private dbConnect = async (): Promise<void> => {
+    await db.$connect().catch(async () => {
+      throw prismaConnectionError();
+    });
+  };
+
+  public readonly authorize = async (
+    loginProps: LoginProps
+  ): Promise<UserLogged | null> => {
+    await this.dbConnect();
+    const user = await this.userRepository.listByEmail(loginProps.email);
+
+    if (user) {
+      return {
+        id: user?.id,
+        name: user?.name,
+        email: user?.email,
+        role: user?.role,
+        password: user?.password,
+      } as UserLogged;
+    }
+
+    return null;
+  };
+}
--- src/backend/infra/factories/services/auth/login.ts
+++ src/backend/infra/factories/services/auth/login.ts
@@ -1,19 +1,19 @@
 import {
-  AuthRepositoryInMemory,
-  UserRepositoryInMemory,
-} from "@/backend/infra/in-memory-repositories";
-import {
   EmailValidatorStub,
   EncrypterStub,
   TokenHandlerStub,
 } from "@/backend/data/__mocks__";
+import {
+  PrismaAuthRepositoryAdapter,
+  PrismaUserRepositoryAdapter,
+} from "@/backend/infra/adapters/prisma";
 import { LoginService } from "@/backend/data/services";
 import { LoginValidator } from "@/backend/data/validators";
 import { ValidationErrors } from "@/backend/data/helpers";
 
 export const makeLoginService = (): LoginService => {
-  const userRepository = new UserRepositoryInMemory();
-  const authRepository = new AuthRepositoryInMemory(userRepository);
+  const userRepository = new PrismaUserRepositoryAdapter();
+  const authRepository = new PrismaAuthRepositoryAdapter(userRepository);
   const emailValidator = new EmailValidatorStub();
   const encrypter = new EncrypterStub();
   const validationErrors = new ValidationErrors();
