2430eec73ed9df38bc92b50ab90850d1bae367a8
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-02-27 13:43:21 -0300

    test: ensure that UserCreateValidator throw if password is not provided

--- src/backend/domain/validators/user/__tests__/user.create.validator.spec.ts
+++ src/backend/domain/validators/user/__tests__/user.create.validator.spec.ts
@@ -67,4 +67,17 @@ describe("UserCreateValidator", () => {
       } as UserCreateData),
     ).rejects.toThrow(new MissingParamError("data de nascimento"));
   });
+
+  it("should throw a MissingParamError if password is not provided", async () => {
+    const { sut } = makeSut();
+
+    await expect(
+      sut.validate({
+        name: "any_name",
+        email: "any_email",
+        phone: "any_phone",
+        password: "any_password",
+      } as UserCreateData),
+    ).rejects.toThrow(new MissingParamError("data de nascimento"));
+  });
 });
