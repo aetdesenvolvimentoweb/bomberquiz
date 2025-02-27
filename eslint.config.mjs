7307832b03f825cda1592559be6cd3dff8f760d7
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-02-27 11:48:42 -0300

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,23 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    plugins: ["react-hooks", "@next/next", "prettier"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "sort-imports": [
+        "error",
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+          allowSeparatedGroups: false,
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
