fa2067715c3b0cdd2e731bb3c8d033f0dbbd8c32
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-02-27 13:36:51 -0300

    test: ensure that UserCreateValidator throw if no email is provided

--- src/backend/domain/validators/user/__tests__/user.create.validator.spec.ts
+++ src/backend/domain/validators/user/__tests__/user.create.validator.spec.ts
@@ -28,4 +28,17 @@ describe("UserCreateValidator", () => {
       } as UserCreateData),
     ).rejects.toThrow(new MissingParamError("nome"));
   });
+
+  it("should throw a MissingParamError if email is not provided", async () => {
+    const { sut } = makeSut();
+
+    await expect(
+      sut.validate({
+        name: "any_name",
+        phone: "any_phone",
+        birthdate: new Date(),
+        password: "any_password",
+      } as UserCreateData),
+    ).rejects.toThrow(new MissingParamError("email"));
+  });
 });
