59cfa1c9d7c6b07095cb9a167ab89676b98af796
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-02-27 12:11:51 -0300

    feat: create custom application error

---
+++ src/backend/domain/errors/application.error.ts
@@ -0,0 +1,14 @@
+/**
+ * Classe base para erros da aplicação que inclui código de status HTTP
+ * @param message Mensagem descritiva do erro
+ * @param statusCode Código de status HTTP do erro
+ */
+export class ApplicationError extends Error {
+  constructor(
+    message: string,
+    readonly statusCode: number,
+  ) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
