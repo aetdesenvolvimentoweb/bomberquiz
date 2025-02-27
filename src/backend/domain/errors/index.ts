08ea693a8570ff81a03cf78a7da6b11b0379e54e
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-02-27 12:18:47 -0300

    feat: organize exports

---
+++ src/backend/domain/errors/index.ts
@@ -0,0 +1,5 @@
+export * from "./application.error";
+export * from "./database.error";
+export * from "./duplicate.resource.error";
+export * from "./invalid.param.error";
+export * from "./missing.param.error";
