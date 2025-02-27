1e94b72ea39cd21c407c530da6601d82084aee13
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-02-27 13:24:24 -0300

    test: ensure that UserCreateValidator throw if no name is provided

---
+++ src/backend/domain/validators/user/__tests__/user.create.validator.spec.ts
@@ -0,0 +1,31 @@
+import { MissingParamError } from "@/backend/domain/errors";
+import { UserCreateData } from "@/backend/domain/entities";
+import { UserCreateValidator } from "@/backend/data/validators/user/user.create.validator";
+import { UserCreateValidatorUseCase } from "../user.create.validator";
+
+interface SutResponses {
+  sut: UserCreateValidatorUseCase;
+}
+
+const makeSut = (): SutResponses => {
+  const sut = new UserCreateValidator();
+
+  return {
+    sut,
+  };
+};
+
+describe("UserCreateValidator", () => {
+  it("should throw a MissingParamError if name is not provided", async () => {
+    const { sut } = makeSut();
+
+    await expect(
+      sut.validate({
+        email: "any_email",
+        phone: "any_phone",
+        birthdate: new Date(),
+        password: "any_password",
+      } as UserCreateData),
+    ).rejects.toThrow(new MissingParamError("nome"));
+  });
+});
