0e4620d4d3bf8d92303ca125534c201fe8f415b0
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-06 18:34:33 -0300

    feat: create user entity

---
+++ src/backend/domain/entities/user.ts
@@ -0,0 +1,65 @@
+/**
+ * Enum que define os tipos de papéis de usuário no sistema
+ * @enum {string}
+ */
+export enum UserRole {
+  ADMINISTRADOR = "administrador",
+  COLABORADOR = "colaborador",
+  CLIENTE = "cliente",
+}
+
+/**
+ * Interface que define a estrutura completa de um usuário
+ * @interface
+ */
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  birthdate: Date;
+  avatarUrl?: string;
+  role?: UserRole;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/** Type que representa um usuário sem o campo password */
+export type UserMapped = Omit<User, "password">;
+
+/** Type que define os dados necessários para criar um usuário */
+export type UserCreateData = Omit<
+  User,
+  "id" | "avatarUrl" | "role" | "createdAt" | "updatedAt"
+>;
+
+/** Type que define os dados para atualização de avatar */
+export type UserUpdateAvatarData = {
+  id: string;
+  avatarUrl: string;
+};
+
+/** Type que define os dados para atualização de senha */
+export type UserUpdatePasswordData = {
+  id: string;
+  currentPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+};
+
+/** Type que define os dados para atualização do perfil */
+export type UserUpdateProfileData = Omit<
+  User,
+  "avatarUrl" | "role" | "password" | "createdAt" | "updatedAt"
+>;
+
+/** Type que define os dados para atualização do papel do usuário */
+export type UserUpdateRoleData = {
+  id: string;
+  role: UserRole;
+};
+
+export const USER_DEFAULT_AVATAR_URL =
+  "/src/frontend/assets/images/default-avatar.png";
+export const USER_DEFAULT_ROLE = UserRole.CLIENTE;
