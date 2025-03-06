971797b561fea0f23fe35555bef9f22f27a3a48d
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-06 18:28:13 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  collectCoverage: false,
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/backend/**/*.ts"],
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "js"],
+  moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" },
+  passWithNoTests: true,
+  noStackTrace: true,
+  maxWorkers: 1,
+};
+
+export default config;
