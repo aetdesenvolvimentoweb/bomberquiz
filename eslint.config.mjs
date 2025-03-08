b9489cfd54e37bd5e36160e4a478e73ad3d3f464
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-08 11:05:48 -0300

    chore: add unused-imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,29 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    plugins: ["react-hooks", "@next/next", "simple-import-sort", "prettier"],
+    plugins: [
+      "react-hooks",
+      "@next/next",
+      "simple-import-sort",
+      "unused-imports",
+      "prettier",
+    ],
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
       "prettier/prettier": "error",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-unused-vars": 1,
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   }),
 ];
