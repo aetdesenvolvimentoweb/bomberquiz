0b88d1b5e8d55d0678c78c6866d8723b744314ec
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-18 18:25:50 -0300

    feat: implement logger-provider with console.log

---
+++ src/backend/infra/providers/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Módulo de exportação para implementações de providers
+ *
+ * Este arquivo centraliza a exportação de todas as implementações concretas
+ * dos contratos de providers definidos na camada de domínio.
+ *
+ * @module infra/providers
+ */
+
+export * from "./console-logger";
