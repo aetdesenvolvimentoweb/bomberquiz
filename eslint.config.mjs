648f184631204f7d1755006326afad7ffb2ca397
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-18 14:13:45 -0300

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    plugins: ["react-hooks", "@next/next", "prettier"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "prettier/prettier": "error",
+    },
+  }),
 ];
 
 export default eslintConfig;
