bbff82ce282b2029d731cad1de2ad324fc848bfe
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-18 18:12:21 -0300

    feat: create an user unique email validator

--- src/backend/domain/validators/user/index.ts
+++ src/backend/domain/validators/user/index.ts
@@ -34,3 +34,4 @@ export * from "./user-birthdate-validator";
 export * from "./user-phone-validator";
 export * from "./user-email-validator";
 export * from "./user-password-validator";
+export * from "./user-unique-email-validator";
