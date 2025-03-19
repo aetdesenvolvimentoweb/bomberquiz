f9af84b62d0dbff6b7eb5aa9d350c0c8ed0eaf1c
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-18 22:01:47 -0300

    feat: implements password validator with password-validator lib

---
+++ src/backend/infra/adapters/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Módulo de exportação para adaptadores de bibliotecas externas
+ *
+ * Este arquivo centraliza a exportação de todos os adaptadores que integram
+ * bibliotecas de terceiros com as interfaces definidas no domínio da aplicação.
+ *
+ * @module infra/adapters
+ */
+
+export * from "./password-validator-adapter";
