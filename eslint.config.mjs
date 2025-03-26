8a29879ce1a393951c43379d8df2bb3e599506c8
AET Desenvolvimento <aetdesenvolvimentoweb@gmail.com>
2025-03-26 11:18:05 -0300

    chore: initialize project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,47 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    plugins: [
+      "react-hooks",
+      "@next/next",
+      "react",
+      "jsx-a11y",
+      "simple-import-sort",
+      "unused-imports",
+      "prettier",
+    ],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-img-element": "error",
+      "@next/next/no-html-link-for-pages": "error",
+      "jsx-a11y/alt-text": "error",
+      "jsx-a11y/anchor-has-content": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "sort-imports": "off",
+      "import/order": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          // Isso faz com que importações não utilizadas também sejam detectadas
+          caughtErrors: "all",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "unused-imports/no-unused-imports": "error",
+    },
+  }),
 ];
 
 export default eslintConfig;
